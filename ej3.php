83867d69d864bc15dc4632d087e405ffd21772ed
Manuel51O <a21goruma@iesgrancapitan.org>
2023-05-21 02:51:51 +0200

    php_3 Manuel51O Ejercicio_3 commit

---
+++ ej3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario</title>
+</head>
+<body>
+    <form method="post" action="procesar_formulario.php">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
